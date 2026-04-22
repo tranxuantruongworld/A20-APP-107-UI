d3905bdfdc2d3656ca1340aa76ed2e4eb33cd4fb
BanBannBannn <tranvangiaban@gmail.com>
2026-04-22 23:49:55 +0700

    fix api

--- src/schemas/guest_schema.py
+++ src/schemas/guest_schema.py
@@ -2,6 +2,4 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 class GuestJoinSchema(BaseModel):
-    email: EmailStr
     full_name: str
-    organization: Optional[str] = None
