c72bf9ab7782704c473fae50bf6cedfbd5d26a20
tranxuantruongworld <tranxuantruong.world@gmail.com>
2026-04-25 11:09:05 +0700

    feat(ui): tracking ui interaction user

---
+++ ui/components/Clarity.tsx
@@ -0,0 +1,19 @@
+// components/Clarity.tsx
+'use client'
+
+import Script from 'next/script'
+
+export default function MicrosoftClarity() {
+  const CLARITY_ID = process.env.CLARITY_ID || "YOUR_PROJECT_ID_HERE"
+  return (
+    <Script id="microsoft-clarity" strategy="afterInteractive">
+      {`
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+a;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "${CLARITY_ID}");
+      `}
+    </Script>
+  )
+}
