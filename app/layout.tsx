ee708ffde0e9dd15c33f180d8e67b4d6668634f8
tranxuantruongworld <tranxuantruong.world@gmail.com>
2026-04-25 11:09:05 +0700

    feat(ui): tracking ui interaction user

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import MicrosoftClarity from '@/components/Clarity'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +29,10 @@ export default function RootLayout({
         lang="en"
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-background`}
       >
-        <body className="min-h-full flex flex-col">{children}</body>
+        <body className="min-h-full flex flex-col">
+          <MicrosoftClarity />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
